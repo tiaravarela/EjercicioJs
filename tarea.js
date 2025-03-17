f7d11188778841855a0fa4d61c8a94227be43f92
tiaravarela <tiarasandoval01@gmail.com>
2025-03-17 17:01:35 -0300

    mi primer commit

---
+++ tarea.js
@@ -0,0 +1,21 @@
+function eje1() {
+
+var valores = [true, 5, false, "hola", "adios", 2];
+   
+ if (valores[4].length > valores[3].length)
+ {
+    console.log ("el valor "+valores[4]+ "es mayor que "+valores[3]);
+    console.log (valores[0]);
+ }
+ else{
+    console.log ("el valor "+valores[3]+ " es mayor que "+valores[4]);
+    console.log (valores[2]);
+ }
+ console.log(valores[1]+valores[5])
+ console.log(valores[1]-valores[5])
+ console.log(valores[1]/valores[5])
+ console.log(valores[1]*valores[5])
+ console.log(valores[1]%valores[5])
+ 
+ 
+ }
